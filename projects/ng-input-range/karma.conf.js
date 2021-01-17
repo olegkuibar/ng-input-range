47f3d8e62416cef359b40908b0e47e81335c58cf
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-01-17 22:34:15 +0300

    chore(project/lib): update karma.conf.js and package.json

--- projects/ng-input-range/karma.conf.js
+++ projects/ng-input-range/karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     autoWatch: false,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
   });
 };
