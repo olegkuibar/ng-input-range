02fc2f9f71ae4a734b80e3f217770815b70b1d55
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-01-17 21:54:53 +0300

    test(lib): add test for mat-form-field appearance

--- projects/ng-input-range/src/lib/ng-input-range.component.spec.ts
+++ projects/ng-input-range/src/lib/ng-input-range.component.spec.ts
@@ -1,25 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Provider, Type, ViewChild } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldAppearance, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { PlatformModule } from '@angular/cdk/platform';
+import { NgInputRangeModule } from 'ng-input-range';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
-import { NgInputRangeComponent } from './ng-input-range.component';
+describe('NgInputRange', () => {
+  function createComponent<T>(component: Type<T>, providers: Provider[] = []): ComponentFixture<T> {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        PlatformModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        NgInputRangeModule,
+      ],
+      declarations: [component],
+      providers,
+    }).compileComponents();
 
-describe('NgInputRangeComponent', () => {
-  let component: NgInputRangeComponent;
-  let fixture: ComponentFixture<NgInputRangeComponent>;
+    return TestBed.createComponent<T>(component);
+  }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NgInputRangeComponent ]
-    })
-    .compileComponents();
+  describe('NgInputRange with appearance', () => {
+    const nonLegacyAppearances: MatFormFieldAppearance[] = ['standard', 'fill'];
+    let fixture: ComponentFixture<NgInputRangeWithAppearance>;
+    let testComponent: NgInputRangeWithAppearance;
+    let containerEl: HTMLElement;
+
+    beforeEach(fakeAsync(() => {
+      fixture = createComponent(NgInputRangeWithAppearance);
+      fixture.detectChanges();
+      testComponent = fixture.componentInstance;
+      containerEl = fixture.debugElement.query(By.css('mat-form-field'))!.nativeElement;
+    }));
+
+    it('legacy appearance should promote placeholder to label', fakeAsync(() => {
+      testComponent.appearance = 'legacy';
+      fixture.detectChanges();
+
+      expect(containerEl.classList).toContain('mat-form-field-appearance-legacy');
+      expect(testComponent.formField._hasFloatingLabel()).toBe(false);
+      expect(testComponent.formField._hideControlPlaceholder()).toBe(true);
+    }));
+
+    it('non-legacy appearances should not promote placeholder to label', fakeAsync(() => {
+      for (let appearance of nonLegacyAppearances) {
+        testComponent.appearance = appearance;
+        fixture.detectChanges();
+
+        expect(containerEl.classList).toContain(`mat-form-field-appearance-${appearance}`);
+        expect(testComponent.formField._hasFloatingLabel()).toBe(false);
+        expect(testComponent.formField._hideControlPlaceholder()).toBe(false);
+      }
+    }));
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgInputRangeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  @Component({
+    template: `
+      <mat-form-field [appearance]="appearance" floatLabel="never">
+        <ng-input-range></ng-input-range>
+      </mat-form-field>
+    `,
+  })
+  class NgInputRangeWithAppearance {
+    @ViewChild(MatFormField) formField: MatFormField;
+    appearance: MatFormFieldAppearance;
+  }
 });
