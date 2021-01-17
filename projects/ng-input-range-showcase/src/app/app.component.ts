87980d8c35bbab02404e384baf9b69eea45fc7d3
Oleg Kuibar <kuibar.oleg@gmail.com>
2021-01-17 22:55:38 +0300

    fix(showcase): fix build -> update path to NgInputRangeComponent

--- projects/ng-input-range-showcase/src/app/app.component.ts
+++ projects/ng-input-range-showcase/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgInputRangeComponent } from 'ng-input-range';
 import { FormControl, FormGroup } from '@angular/forms';
+import { NgInputRangeComponent } from '../../../ng-input-range/src/lib/ng-input-range.component';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,7 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  get value(): number {
-    return this._value;
-  }
-
-  set value(value: number) {
-    this._value = value;
-    this.form.setValue({ inputRange: value });
-  }
   @ViewChild(NgInputRangeComponent) inputRange: NgInputRangeComponent;
-
   title = 'ng-input-range-showcase';
   autoTicks = false;
   disabled = false;
@@ -26,14 +17,23 @@ export class AppComponent {
   step = 1;
   appearance: 'fill' | 'outline' = 'outline';
   theme: 'primary' | 'accent' = 'primary';
-
   form = new FormGroup({
     inputRange: new FormControl({ value: 96, disabled: this.disabled }),
   });
-
   eventCount = 0;
 
   private _value = 0;
+
+  get value(): number {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._value;
+  }
+
+  set value(value: number) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._value = value;
+    this.form.setValue({ inputRange: value });
+  }
 
   increaseEventCount(): void {
     this.eventCount++;
